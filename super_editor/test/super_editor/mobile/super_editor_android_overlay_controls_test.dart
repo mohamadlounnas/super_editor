a0b97ccbf1869a5bf72f4a3fd14eb96e2f23ee49
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2025-02-19 20:49:50 -0300

    Use TestSuperKeyboard in tests

--- super_editor/test/super_editor/mobile/super_editor_android_overlay_controls_test.dart
+++ super_editor/test/super_editor/mobile/super_editor_android_overlay_controls_test.dart
@@ -6,6 +6,7 @@ import 'package:follow_the_leader/follow_the_leader.dart';
 import 'package:super_editor/src/infrastructure/platforms/android/selection_handles.dart';
 import 'package:super_editor/super_editor.dart';
 import 'package:super_editor/super_editor_test.dart';
+import 'package:super_keyboard/test/keyboard_simulator.dart';
 import 'package:super_text_layout/super_text_layout.dart';
 
 import '../../test_runners.dart';
@@ -174,6 +175,11 @@ void main() {
           .createDocument()
           .withSingleEmptyParagraph()
           .pump();
+
+      // The decision about showing the toolbar depends on the keyboard visibility.
+      // Simulate the keyboard being visible immediately after the IME is connected.
+      TestSuperKeyboard.install(tester, keyboardAnimationTime: Duration.zero);
+      addTearDown(() => TestSuperKeyboard.uninstall());
 
       // Ensure the toolbar is not visible.
       expect(SuperEditorInspector.isMobileToolbarVisible(), isFalse);
